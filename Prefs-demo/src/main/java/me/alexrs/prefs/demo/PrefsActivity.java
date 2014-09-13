effd9b514185058fd85de7790fde1ed1e3fecbdf
Alejandro Rodríguez Salamanca <alexrs95@gmail.com>
2014-09-13 21:33:09 +0200

    Updated demo

--- Prefs-demo/src/main/java/me/alexrs/prefs/demo/PrefsActivity.java
+++ Prefs-demo/src/main/java/me/alexrs/prefs/demo/PrefsActivity.java
@@ -14,10 +14,28 @@ public class PrefsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.a_prefs);
-        TextView tv = (TextView) findViewById(R.id.textView);
 
-        Prefs.with(this).save("NAME", "Hector");
-        tv.setText(Prefs.with(this).getString("NAME", ""));
+        TextView tv1 = (TextView) findViewById(R.id.textView1);
+        TextView tv2 = (TextView) findViewById(R.id.textView2);
+        TextView tv3 = (TextView) findViewById(R.id.textView3);
+        TextView tv4 = (TextView) findViewById(R.id.textView4);
+        TextView tv5 = (TextView) findViewById(R.id.textView5);
+
+
+        Prefs.with(this).save("STRING", "HEY PREFS!");
+        tv1.setText("String: " + Prefs.with(this).getString("STRING", ""));
+
+        Prefs.with(this).save("BOOL", true);
+        tv2.setText("Boolean: " + String.valueOf(Prefs.with(this).getBoolean("BOOL", false)));
+
+        Prefs.with(this).save("INT", 42);
+        tv3.setText("Int: " + String.valueOf(Prefs.with(this).getInt("INT", 0)));
+
+        Prefs.with(this).save("FLOAT", 1.0F);
+        tv4.setText("Float: " + String.valueOf(Prefs.with(this).getFloat("FLOAT", 0F)));
+
+        Prefs.with(this).save("LONG", 36352353252135L);
+        tv5.setText("Long: " + String.valueOf(Prefs.with(this).getLong("LONG", 0)));
     }
 
 }
